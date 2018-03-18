66fb887c260e628de035192db41e30a41b93817b
Rahul Gupta <rahul.gupta@utexas.edu>
2018-03-18 14:50:32 -0500

    Updated Name

--- src/assignment4/Main.java
+++ src/assignment4/Main.java
@@ -2,8 +2,8 @@ package assignment4;
 /* CRITTERS Main.java
  * EE422C Project 4 submission by
  * Replace <...> with your actual data.
- * <Student1 Name>
- * <Student1 EID>
+ * <Rahul Gupta>
+ * <rg43226>
  * <Student1 5-digit Unique No.>
  * <Student2 Name>
  * <Student2 EID>
